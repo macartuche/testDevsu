0b8a8ff88f46c2f58fffc9c0547eb078c04ec203
macartuche <macartuche@gmail.com>
2026-04-22 21:51:15 -0500

    Corregir Postman collection JSON syntax y excluir test de integracion

--- backend/src/test/java/ec/gob/loja/devsu/backend/BackendApplicationTests.java
+++ backend/src/test/java/ec/gob/loja/devsu/backend/BackendApplicationTests.java
@@ -3,7 +3,9 @@ package ec.gob.loja.devsu.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// Integration test - requires running PostgreSQL
+// Run manually: mvn test -Dspring.profiles.active=default
+// @SpringBootTest
 class BackendApplicationTests {
 
 	@Test
